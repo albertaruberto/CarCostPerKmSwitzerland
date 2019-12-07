8dfb784078239fc64bd9553ea73c7bc385462dab
Albert Previ <36011199+albertaruberto@users.noreply.github.com>
2019-12-07 11:50:50 +0100

    Add files via upload

---
+++ Car Cost per Km Switzerland.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+drivingForeignCountries = 800
+drivingcarToWork = 6
+others = 15
+oneYear = 12
+totalKm = (drivingForeignCountries + drivingcarToWork + others)
+totalKmInOneYear = (totalKm * oneYear)
+print(totalKmInOneYear)
+
+carCostPerKm = 0.70
+carCostperKmOneYear = totalKmInOneYear * carCostPerKm
+
+print(carCostperKmOneYear)
+
+
+# In[ ]:
+
+
+
+
